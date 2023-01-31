19ef31e99a22988b7bd79e1b7c27063ba3ac7f84
kratos-te <michaelgordon.kratos@gmail.com>
2023-02-01 03:15:47 +0900

    version1

---
+++ src/router.tsx
@@ -0,0 +1,192 @@
+import { Suspense, lazy } from 'react';
+import { Navigate } from 'react-router-dom';
+import { RouteObject } from 'react-router';
+
+import SidebarLayout from 'src/layouts/SidebarLayout';
+import BaseLayout from 'src/layouts/BaseLayout';
+
+import SuspenseLoader from 'src/components/SuspenseLoader';
+
+const Loader = (Component) => (props) => (
+  <Suspense fallback={<SuspenseLoader />}>
+    <Component {...props} />
+  </Suspense>
+);
+
+// Pages
+
+const Overview = Loader(lazy(() => import('src/content/overview')));
+
+// Dashboards
+
+const Home = Loader(lazy(() => import('src/content/dashboards/Home')));
+
+// Applications
+
+const Messenger = Loader(
+  lazy(() => import('src/content/applications/Messenger'))
+);
+const Table = Loader(lazy(() => import('src/content/applications/Table')));
+const UserProfile = Loader(
+  lazy(() => import('src/content/applications/Users/profile'))
+);
+const UserSettings = Loader(
+  lazy(() => import('src/content/applications/Users/settings'))
+);
+
+// Components
+
+const Buttons = Loader(
+  lazy(() => import('src/content/pages/Components/Buttons'))
+);
+const Modals = Loader(
+  lazy(() => import('src/content/pages/Components/Modals'))
+);
+const Accordions = Loader(
+  lazy(() => import('src/content/pages/Components/Accordions'))
+);
+const Tabs = Loader(lazy(() => import('src/content/pages/Components/Tabs')));
+const Badges = Loader(
+  lazy(() => import('src/content/pages/Components/Badges'))
+);
+const Tooltips = Loader(
+  lazy(() => import('src/content/pages/Components/Tooltips'))
+);
+const Avatars = Loader(
+  lazy(() => import('src/content/pages/Components/Avatars'))
+);
+const Cards = Loader(lazy(() => import('src/content/pages/Components/Cards')));
+const Forms = Loader(lazy(() => import('src/content/pages/Components/Forms')));
+
+// Status
+
+const Status404 = Loader(
+  lazy(() => import('src/content/pages/Status/Status404'))
+);
+const Status500 = Loader(
+  lazy(() => import('src/content/pages/Status/Status500'))
+);
+const StatusComingSoon = Loader(
+  lazy(() => import('src/content/pages/Status/ComingSoon'))
+);
+const StatusMaintenance = Loader(
+  lazy(() => import('src/content/pages/Status/Maintenance'))
+);
+
+const routes: RouteObject[] = [
+  {
+    path: '',
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to="home" replace />
+      },
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'messenger',
+        element: <Messenger />
+      }
+    ]
+  },
+  {
+    path: 'dashboards',
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to="home" replace />
+      },
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'messenger',
+        element: <Messenger />
+      }
+    ]
+  },
+  {
+    path: 'management',
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to="table" replace />
+      },
+      {
+        path: 'table',
+        element: <Table />
+      },
+      {
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            element: <Navigate to="details" replace />
+          },
+          {
+            path: 'details',
+            element: <UserProfile />
+          },
+          {
+            path: 'settings',
+            element: <UserSettings />
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/components',
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to="buttons" replace />
+      },
+      {
+        path: 'buttons',
+        element: <Buttons />
+      },
+      {
+        path: 'modals',
+        element: <Modals />
+      },
+      {
+        path: 'accordions',
+        element: <Accordions />
+      },
+      {
+        path: 'tabs',
+        element: <Tabs />
+      },
+      {
+        path: 'badges',
+        element: <Badges />
+      },
+      {
+        path: 'tooltips',
+        element: <Tooltips />
+      },
+      {
+        path: 'avatars',
+        element: <Avatars />
+      },
+      {
+        path: 'cards',
+        element: <Cards />
+      },
+      {
+        path: 'forms',
+        element: <Forms />
+      }
+    ]
+  }
+];
+
+export default routes;
